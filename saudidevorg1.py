747be62bc80001411c54b07724b04a150b0a0554
arwagh <arwalghamdi@gmail.com>
2019-08-23 12:50:48 +0300

    Variables on Python

--- saudidevorg1.py
+++ saudidevorg1.py
@@ -6,3 +6,33 @@ of comments
 print("Hello World")
 if 5 > 2:
     print("five is greater than two!")
+
+w = "CS"
+print(w)
+w = 'CS'
+print(w)
+
+x = 5
+y = "Arwa"
+y = 8
+print(x)
+print(y)
+
+a, b, c = "A", "B", "C"
+print(a)
+print(b)
+print(c)
+
+a = b = c = "Letter"
+print(a)
+print(b)
+print(c)
+print("It's a " + a)
+
+a = "Python is"
+b = "awesome"
+print(a + b)
+
+a = 5
+b = 8
+print(a+b)
