a0d520eedfddc4648540a664ecc3472f4f95d3c3
arwagh <arwalghamdi@gmail.com>
2019-08-27 20:59:42 +0300

    String in Python

--- saudidevorg1.py
+++ saudidevorg1.py
@@ -1,16 +1,8 @@
-a = "Hello"
-b = 'Hello'
+a = " Hello, World!"
 
-print(a)
-print(b)
-
-a = """This is
-multiple lines
-code in Python
-"""
-print(a)
-
-a = "Hello, World!"
-
-print(a[1])
-print(a[2:5])
+print(a.strip())
+print(len(a))
+print(a.lower())
+print(a.upper())
+print(a.replace("H", "J"))
+print(a.split(","))
