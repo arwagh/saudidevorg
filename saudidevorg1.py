fe9b80fb4bbb2c1a524f6b773c28520ae43011e8
arwagh <arwalghamdi@gmail.com>
2019-09-02 21:42:04 +0300

    Python lists 3

--- saudidevorg1.py
+++ saudidevorg1.py
@@ -18,3 +18,29 @@ print(list)
 list = [3.4,2.12,5.7]
 list = list+numbersList
 print(list)
+print(len(list))
+list.append("A-Z")
+list.insert(0, "Hello World")
+print(list[0])
+
+print(list)
+
+list.remove(3.4)
+print(list)
+
+list.pop()
+print(list)
+list.pop(3)
+print(list)
+
+#list.clear()
+print(list)
+#list = numbersList
+
+#list = numbersList.copy()
+
+list1 = list(numbersList)
+print(list1)
+
+newList = list(("a", "b", "c"))
+print(newList)
