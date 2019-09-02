d6458d798af8c4e1e466018bebfa691ff5728008
arwagh <arwalghamdi@gmail.com>
2019-09-02 17:22:40 +0300

    Python lists 2

--- saudidevorg1.py
+++ saudidevorg1.py
@@ -10,3 +10,11 @@ for x in numbersList:
 
 del numbersList[0]
 print(numbersList[0])
+
+list = ["a", "b", "c", "d", 1, 2]
+print(list[1:3])
+list = ["Python"]* 5
+print(list)
+list = [3.4,2.12,5.7]
+list = list+numbersList
+print(list)
