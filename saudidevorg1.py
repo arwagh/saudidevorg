ecdc8761b68e26c3db40a68b021e8db10aaa9245
arwagh <arwalghamdi@gmail.com>
2019-09-12 12:42:51 +0300

    Python lists 3

--- saudidevorg1.py
+++ saudidevorg1.py
@@ -1,46 +1,29 @@
-numbersList = [1,2,3,4,5]
-print(numbersList)
-print(numbersList[0])
-print(numbersList[1])
-print(numbersList[-1])
+list1 = [1,2,3,4]
 
-numbersList[2] = 12
-for x in numbersList:
-    print(x)
+newList = list1.copy()
 
-del numbersList[0]
-print(numbersList[0])
+print(newList)
 
-list = ["a", "b", "c", "d", 1, 2]
-print(list[1:3])
-list = ["Python"]* 5
-print(list)
-list = [3.4,2.12,5.7]
-list = list+numbersList
-print(list)
-print(len(list))
-list.append("A-Z")
-list.insert(0, "Hello World")
-print(list[0])
+list1.pop(0)
 
-print(list)
+print(list1)
+print(newList)
 
-list.remove(3.4)
-print(list)
+newList = list1
 
-list.pop()
-print(list)
-list.pop(3)
-print(list)
+list1.pop(0)
+print(list1)
+print(newList)
 
-#list.clear()
-print(list)
-#list = numbersList
-
-#list = numbersList.copy()
-
-list1 = list(numbersList)
+list1.insert(0,1)
+list1.insert(1,2)
 print(list1)
 
-newList = list(("a", "b", "c"))
+
+newList = list(list1)
+list1.pop(0)
+print(newList)
+
+newList = list((1,2,3,4,5))
+
 print(newList)
