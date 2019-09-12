600cd7d5c62ab2c7ade429b90f740b0b43eea4f8
arwagh <arwalghamdi@gmail.com>
2019-09-12 16:37:32 +0300

    Python Dictionaries 2

--- saudidevorg1.py
+++ saudidevorg1.py
@@ -1,5 +1,7 @@
 thisDict = {1:"a",
             2:"b"}
+
+"""
 print(thisDict)
 print(thisDict[1])
 print(thisDict.get(1))
@@ -7,7 +9,7 @@ print(thisDict.get(1))
 thisDict[1] = 'c'
 
 print(thisDict)
-
+"""
 
 for x in thisDict:
     print(x)
@@ -20,3 +22,24 @@ for x in thisDict.values():
 
 for x, y in thisDict.items():
     print(x, y)
+
+
+print(4 in thisDict)
+
+print(len(thisDict))
+
+thisDict[3] = "d"
+
+print(thisDict)
+
+#thisDict.pop(2)
+
+print(thisDict)
+
+thisDict.popitem()
+
+print(thisDict)
+
+del thisDict[1]
+
+print(thisDict)
