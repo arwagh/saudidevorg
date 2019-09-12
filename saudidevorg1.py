d9f3197aba0aea5962835b1649052dddd4080d7b
arwagh <arwalghamdi@gmail.com>
2019-09-12 14:01:50 +0300

    Pyhton sets 2

--- saudidevorg1.py
+++ saudidevorg1.py
@@ -8,3 +8,18 @@ for x in thisSet:
 thisSet.update([123,212,'ABC'])
 
 print(thisSet)
+
+print(len(thisSet))
+
+thisSet.remove(123)
+thisSet.discard(212)
+
+print(thisSet)
+
+thisSet.pop()
+
+print(thisSet)
+
+thisSet.clear()
+
+print(thisSet)
