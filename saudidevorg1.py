6fb25f391d1160e2ab0c7587b7a14f183775afd1
arwagh <arwalghamdi@gmail.com>
2019-09-12 13:26:21 +0300

    Python tuples 2

--- saudidevorg1.py
+++ saudidevorg1.py
@@ -10,3 +10,19 @@ for x in thisTuple:
     print(x)
 
 print(thisTuple[0:4])
+
+print(3.1 in thisTuple)
+
+thisTuple = (2,)*6
+newTuple = (3,5,"a")
+newestTuple = thisTuple + (4234,4234,21)
+print(newestTuple)
+
+print(len(thisTuple))
+
+thisTuple = tuple((1,2,4,5))
+
+print(thisTuple)
+
+thisList = [1432,32,23]
+print(tuple(thisList))
