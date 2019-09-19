9226d1dd7f7ad3dabe1c582f36c74aa0f11aa345
arwagh <arwalghamdi@gmail.com>
2019-09-19 12:23:03 +0300

    Week 4 challenge

---
+++ week4Challenge.py
@@ -0,0 +1,21 @@
+thisSet = {1,3,5,7,8}
+print(thisSet)
+thisSet.remove(8)
+
+print(thisSet)
+
+thisSet.clear()
+
+print(thisSet)
+
+thisDict = {"name" : "pigeon",
+            "type" : "bird",
+            "skin cover" : "feathers"}
+
+print(thisDict)
+
+print(thisDict["type"])
+
+thisDict["skin cover"] = "skin"
+
+print(thisDict)
