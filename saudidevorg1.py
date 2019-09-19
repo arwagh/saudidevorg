1f64dcfed3ce62cf93b5463a87cecfb2bf72b228
arwagh <arwalghamdi@gmail.com>
2019-09-19 13:01:07 +0300

    Python loops - while

--- saudidevorg1.py
+++ saudidevorg1.py
@@ -1,14 +1,9 @@
-if (5>7): print("true 1")
-elif(4>2): print("true 2")
-else: print("false")
-
-print("true") if("a" in "name") else print("false")
-
-print("TRUE") if ("c" in "name") else print("FALSE") if (4<2) else print("NO")
-
-if(4>2) or (5<=5):
-    print("Larger")
-
-if (4>2):
-    if(5>3):
-        print("yes")
+i = 5
+j = 4
+while(i < 9):
+    i+=1
+    if(i==7):
+        continue
+    print(i)
+else:
+    print("finished looping")
