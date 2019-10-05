521e3c1828a12fa0b39b2c6dab98eb5f1a867f61
arwagh <arwalghamdi@gmail.com>
2019-10-05 19:47:11 +0300

    Python classes and objects 2

--- saudidevorg1.py
+++ saudidevorg1.py
@@ -3,10 +3,18 @@ class Person:
         self.name=name
         self.age=age
 
-    def myfunc(abc):
-        print("Hello my name is " + abc.name)
+    def myfunc(self):
+        print("Hello my name is " + self.name)
 
 
 
 p1 = Person("John", 36)
 p1.myfunc()
+
+p1.age=40
+print(p1.age)
+
+del p1.age
+
+del p1
+print(p1.myfunc())
